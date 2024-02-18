83c1f0a3a58af7aef71e8c8bf98384dbeb32c3a3
ly <4545541-ly95@users.noreply.gitlab.com>
2024-02-18 14:53:17 +0800

    u

--- libmobile/src/go/mobile/aptos.go
+++ libmobile/src/go/mobile/aptos.go
@@ -1,78 +1,42 @@
 package mobile
 
 import (
+	"encoding/hex"
+
 	"github.com/okx/go-wallet-sdk/coins/aptos"
-	"github.com/okx/go-wallet-sdk/coins/aptos/types"
 	"github.com/okx/go-wallet-sdk/crypto/ed25519"
 )
 
-func APTOS_NewAddress(seedHex string, shortEnable bool) (string, error) {
-	return aptos.NewAddress(seedHex, shortEnable)
-}
-
-func APTOS_GetAddressByPubKey(pubKeyHex string, shortEnable bool) (string, error) {
-	return aptos.GetAddressByPubKey(pubKeyHex, shortEnable)
-}
-
-func APTOS_ValidateAddress(address string, shortEnable bool) bool {
-	return aptos.ValidateAddress(address, shortEnable)
-}
-
-func APTOS_MakeRawTransaction(
-	from string,
-	sequenceNumber uint64,
-	maxGasAmount uint64,
-	gasUnitPrice uint64,
-	expirationTimestampSecs uint64,
-	chainId uint8,
-	payload types.TransactionPayload,
-) *types.RawTransaction {
-	return aptos.MakeRawTransaction(
-		from,
-		sequenceNumber,
-		maxGasAmount,
-		gasUnitPrice,
-		expirationTimestampSecs,
-		chainId,
-		payload,
-	)
-}
-
-func APTOS_BuildSignedTransaction(
-	from string,
-	sequenceNumber uint64,
-	maxGasAmount uint64,
-	gasUnitPrice uint64,
-	expirationTimestampSecs uint64,
-	chainId uint8,
-	payload types.TransactionPayload,
+func APTOS_Swap(
 	seedHex string,
-) (string, error) {
-	return aptos.BuildSignedTransaction(
+	from string,
+	sequenceNumber int64,
+	maxGasAmount int64,
+	gasUnitPrice int64,
+	expirationTimestampSecs int64,
+	chainId int8,
+	payload string,
+	abi string,
+) string {
+	p, _ := aptos.PayloadFromJsonAndAbi(payload, abi)
+	rawTxn := aptos.MakeRawTransaction(
 		from,
-		sequenceNumber,
-		maxGasAmount,
-		gasUnitPrice,
-		expirationTimestampSecs,
-		chainId,
-		payload,
-		seedHex,
+		uint64(sequenceNumber),
+		uint64(maxGasAmount),
+		uint64(gasUnitPrice),
+		uint64(expirationTimestampSecs),
+		uint8(chainId),
+		p,
 	)
-}
 
-func APTOS_SignRawTransaction(rawTxn *types.RawTransaction, seedHex string) (string, error) {
-	return aptos.SignRawTransaction(rawTxn, seedHex)
-}
-
-// 只取签名
-func APTOS_ED25519Sign(rawTxn *types.RawTransaction, seedHex string) ([]byte, error) {
 	message, err := rawTxn.GetSigningMessage()
 	if err != nil {
-		return nil, err
+		return ""
 	}
 	signature, err := ed25519.Sign(seedHex, message)
 	if err != nil {
-		return nil, err
+		return ""
 	}
-	return signature, nil
+
+	return hex.EncodeToString(signature)
 }
